aed00284e91ceb6b85373f544feb607da22a9fe7
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2019-12-12 13:23:47 -0500

    add fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+from add_0 import add as suma
 if __name__ == "__main__":
-    import add_0
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    a = 1
+    b = 2
+print("{:d} + {:d} = {:d}".format(a, b, suma(a, b)))
