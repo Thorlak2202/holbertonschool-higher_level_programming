f2bd110ac49235dbeb6da276c0792636a185060c
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2019-12-12 13:29:08 -0500

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = len(sys.argv)
+    i = len(sys.argv) - 1
     if i == 1:
         print("{:d} argument:".format(i))
         print("{:d}: {}".format(1, sys.argv[1]))
     if i == 0:
         print("{:d} arguments.".format(i))
     if i > 1:
-        print("{:d} arguments:".format(i - 1))
+        print("{:d} arguments:".format(i))
         for j in range(1, len(sys.argv)):
             print("{:d}: {}".format(j, sys.argv[j]))
