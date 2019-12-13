47e6e9dcc5fa0b525189be62cd14813bdd260628
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2019-12-13 14:34:12 -0500

    magic numbrz

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    import magic_calculation_102
+    from magic_calculation_102 import add, sub
     if a < b:
-        c = magic_calculation_102.add(a, b)
+        c = add(a, b)
         for i in range(4, 6):
-            c = magic_calculation_102.add(i, c)
+            c = add(i, c)
             return(c)
     else:
-        return(magic_calculation_102.sub(a, b))
+        return(sub(a, b))
