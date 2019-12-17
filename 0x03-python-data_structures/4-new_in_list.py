34ecd0e02935b64e45fdbf80e27e95aa229bfdf9
Thorlak2202 <darkdrakkar@gmail.com>
2019-12-17 12:41:41 -0500

    index fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,8 @@
 def new_in_list(my_list, idx, element):
     i = len(my_list)
     list_copy = my_list[:]
-    if idx > i or idx < 0:
-        new_list = my_list
-        return new_list
+    if idx >= i or idx < 0:
+        return my_list
     else:
         list_copy[idx] = element
         return list_copy
