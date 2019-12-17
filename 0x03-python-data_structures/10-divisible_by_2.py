414e4fca99e883472b5f2e61807e45b706ea1781
Thorlak2202 <darkdrakkar@gmail.com>
2019-12-17 15:05:37 -0500

    divisible

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    for i in range(0, len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list[i] = True
+        else:
+            new_list[i] = False
+    return new_list
