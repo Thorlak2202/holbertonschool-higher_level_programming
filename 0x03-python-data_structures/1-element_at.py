f4b9bbd1ba907505d8d26090b7c507301cf5cc5d
Thorlak2202 <darkdrakkar@gmail.com>
2019-12-17 12:56:49 -0500

    index fixed fro 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = len(my_list)
-    if idx > i or idx < 0:
+    if idx >= i or idx < 0:
         return None
     else:
         return my_list[idx]
