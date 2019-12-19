7cae769fb5b2120b2e7bfdb3a6de8ba3d9d15582
Thorlak2202 <darkdrakkar@gmail.com>
2019-12-19 15:15:59 -0500

    best evaluating none

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     else:
         return max(a_dictionary)
