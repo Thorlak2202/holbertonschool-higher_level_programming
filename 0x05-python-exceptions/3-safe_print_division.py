e9ce14d8d8b65e58231ac4144f952e76bdd5b93b
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2020-01-08 14:09:27 -0500

    zero

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
     try:
         if a != 0 and b != 0:
             result = a / b
         else:
             result = None
     except ZeroDivisionError:
-        return(None)
         pass
     finally:
         print("Inside result: {}".format(result))
