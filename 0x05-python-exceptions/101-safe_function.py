4ed5a6b78ab2819fe92f6f9b8e8236a53eea1831
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2020-01-08 16:31:26 -0500

    advanced 2

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
         return result
     except ValueError as error:
         print("Exception: {}".format(error), file=sys.stderr)
@@ -17,6 +17,4 @@ def safe_function(fct, *args):
         return(None)
     except IndexError as error:
         print("Exception: {}".format(error), file=sys.stderr)
-        return(None)
-    else:
         return(None)
