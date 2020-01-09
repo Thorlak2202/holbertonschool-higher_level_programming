8da05f1da7e2bcfb431dcb61d80abb5f0fd7d301
Thorlak2202 <darkdrakkar@gmail.com>
2020-01-09 18:10:35 -0500

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """this is a class"""
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+            if type(size) != int:
+                raise TypeError
+            elif size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer", end="")
+            raise TypeError
+        except ValueError:
+            print("size must be >= 0", end="")
+            raise ValueError
+
+    def area(self):
+        return self.__size ** 2
