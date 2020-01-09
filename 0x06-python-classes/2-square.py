05a44d75d041ccab08b250e0ce8c45060e455a1f
Christian David Suárez Guerra <darkdrakkar@gmail.com>
2020-01-09 15:18:00 -0500

    values

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        try:
+            self.__size = size
+            if type(size) != int:
+                raise TypeError
+            elif size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer", end="")
+            raise TypeError
+        except ValueError:
+            print("size must be >= 0", end="")
+            raise ValueError
