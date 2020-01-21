feeafca4cc2ce9d84814b1cd7790ad0e13e1012b
Thorlak2202 <darkdrakkar@gmail.com>
2020-01-21 17:12:30 -0500

    number of lines

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
     with open(filename) as file:
-        count = len((file).readlines(  ))
+        count = len((file).readlines())
         return count
