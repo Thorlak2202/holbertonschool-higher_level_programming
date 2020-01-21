89399b9c660e2a12f190a805529eaf4221630cd5
Thorlak2202 <darkdrakkar@gmail.com>
2020-01-21 17:25:32 -0500

    readfile

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,4 +2,4 @@
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as file:
         text = file.read()
-        print("{}".format(text))
+        print("{}".format(text), end="")
