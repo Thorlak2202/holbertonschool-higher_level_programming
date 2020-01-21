7bbce66ff9f51d87305c2fef15243a3a341e23a4
Thorlak2202 <darkdrakkar@gmail.com>
2020-01-20 21:44:52 -0500

    recangle area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self._height = height
+        self._width = width
+
+    def area(self):
+        return(self._height * self._width)
+
+    def __str__(self):
+        return("[Rectangle] {}/{}".format(self._width, self._height))
