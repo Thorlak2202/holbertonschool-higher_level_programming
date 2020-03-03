cb63417a715655b146641047b4c2526ad33fd2cc
Thorlak2202 <darkdrakkar@gmail.com>
2020-03-03 16:39:39 -0500

    cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,4 @@
---script that creates the database hbtn_0d_usa and the table
+-- script that creates the database hbtn_0d_usa and the table
 -- cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
