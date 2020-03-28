eb51fc55dbe38355d7762b4e2cb0d6473ef7b534
Thorlak2202 <darkdrakkar@gmail.com>
2020-03-28 16:44:12 -0500

    fix code for filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    tables = MySQLdb.connect(host='localhost', user=argv[1],
+    tables = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                              passwd=argv[2], db=argv[3])
 
     cur = tables.cursor()
