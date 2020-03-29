44e53610b46e006eb7be03d8af6759dffeffe6a3
Thorlak2202 <darkdrakkar@gmail.com>
2020-03-29 10:57:38 -0500

    fixes typo

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     cur = tables.cursor()
     cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC")
-    for row in cur:
-        print("{}".format(row))
+    for a in cur:
+        print("{}".format(a))
     cur.close()
     tables.close()
