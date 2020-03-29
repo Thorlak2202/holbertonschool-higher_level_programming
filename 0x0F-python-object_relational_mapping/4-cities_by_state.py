259805caa1fbba0d15fbe9679c4b7005a03170c3
Thorlak2202 <darkdrakkar@gmail.com>
2020-03-29 11:22:04 -0500

    typo fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     cur = tables.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name from cities \
                 JOIN states ON cities.state_id = states.id ORDER BY id ASC")
-    for row in cur:
-        print("{}".format(row))
+    for a in cur:
+        print("{}".format(a))
     cur.close()
     tables.close()
