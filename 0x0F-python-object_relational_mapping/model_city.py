9283e0db08c9504ff8f6300eb5b515b553fa7f2e
Thorlak2202 <darkdrakkar@gmail.com>
2020-03-29 20:47:26 -0500

    pep8 compliant model

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,8 +4,9 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
+
 class City(Base):
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable = False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128))
-    state_id = Column(Integer, ForeignKey("states.id"), nullable = False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
