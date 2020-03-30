3075c683811fea7afacedfdff2b93351aa9ed800
Thorlak2202 <darkdrakkar@gmail.com>
2020-03-29 20:45:50 -0500

    fix nullity

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,6 @@ from model_state import Base
 
 class City(Base):
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable = False)
     name = Column(String(128))
-    state_id = Column(Integer, ForeignKey("states.id"))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable = False)
