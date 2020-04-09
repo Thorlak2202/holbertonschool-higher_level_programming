366e4f07730903f983174d00dd2a344e55e622c0
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-09 13:23:16 -0500

    silent mode#

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a POST request to the URL, and displays body of the response.
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST "$1"
+curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -sX POST "$1"
