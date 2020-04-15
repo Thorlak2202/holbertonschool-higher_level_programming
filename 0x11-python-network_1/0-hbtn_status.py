b903eb3ce05a9484766a450d5a568ce54cef4be4
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-15 13:17:06 -0500

    dunno what happens

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status
 """
-from urllib import request
+import urllib.request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as status:
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as status:
         response = status.read()
         print("Body Response:")
         print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode("utf-8")))
+        print("\t- utf8 content: {}".format(response.decode('utf-8')))
