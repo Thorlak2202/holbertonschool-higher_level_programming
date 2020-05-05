e24379507619fe7ee20fc5d2bc2bd67afe2250ec
Thorlak2202 <darkdrakkar@gmail.com>
2020-05-05 12:49:13 -0500

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-if (process.argv[2] == null) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2], 'is', process.argv[3]);
-}
+console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]);
