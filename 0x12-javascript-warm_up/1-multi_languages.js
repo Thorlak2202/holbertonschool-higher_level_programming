d069864fc339a623ad83612cb8698c0018337cd0
Thorlak2202 <darkdrakkar@gmail.com>
2020-05-05 11:03:09 -0500

    print some lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'Javascript is amazing';
+console.log(myVar);
+console.log(myVar1);
+console.log(myVar2);
