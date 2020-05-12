6be45fbb5fcf8a2157471477e2bf30ef9421d3aa
Thorlak2202 <darkdrakkar@gmail.com>
2020-05-12 13:37:32 -0500

    status return for title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,8 +5,7 @@ require('request').get(url + process.argv[2], function (error, response, body) {
     console.log(error);
   } else if (response.statusCode === 200) {
     console.log(JSON.parse(body).title);
-  }
-  else {
-      console.log('error: ' + response.statusCode)
+  } else {
+    console.log('error: ' + response.statusCode);
   }
 });
