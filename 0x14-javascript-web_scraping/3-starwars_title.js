ccfde2497e9cfc90a4cea045725e146e33833f7b
Thorlak2202 <darkdrakkar@gmail.com>
2020-05-12 13:36:12 -0500

    status return for title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,10 @@ const url = 'https://swapi-api.hbtn.io/api/films/';
 require('request').get(url + process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
+  } else if (response.statusCode === 200) {
     console.log(JSON.parse(body).title);
+  }
+  else {
+      console.log('error: ' + response.statusCode)
   }
 });
